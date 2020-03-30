4f0b39070ea4c1f8cf0dee64e5c40dcd9be8b4c2
Chloe Kudryavtsev <toast@toast.cafe>
2020-03-29 22:42:24 -0400

    update path handling
    ~/bin is high priority
    should be used before /usr and other stuff
    
    the rest is low priority

--- dot_zsh/source/path.zsh
+++ dot_zsh/source/path.zsh
@@ -3,15 +3,6 @@ export DENO_DIR=~/.local/deno
 export GOPATH=~/.local/go
 export LUAROCKS_CONFIG=~/.config/luarocks/config.lua
 
-path+=(
-    ~/bin
-    ~/Applications
-    ~/.local/cargo/bin
-    ~/.local/go/bin
-    ~/.local/yarn/bin
-    ~/.gem/ruby/*/bin(N)
-)
-
 if (( $+commands[luarocks] )); then
     PATH="$PATH:$(luarocks path --lr-bin)" # luarocks puts :s in the output
     eval $(luarocks path --no-bin)
@@ -23,3 +14,15 @@ if (( $+commands[yarn] )) && [[ ! -f ~/.yarnrc ]]; then
     yarn config set global-folder ~/.local/yarn/global
     yarn config set prefix ~/.local/yarn
 fi
+
+# high priority
+path=( ~/bin $path )
+
+# low priority
+path+=(
+    ~/Applications
+    ~/.local/cargo/bin
+    ~/.local/go/bin
+    ~/.local/yarn/bin
+    ~/.gem/ruby/*/bin(N)
+)
