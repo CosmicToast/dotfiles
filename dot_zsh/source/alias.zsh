cc4aba73240c16df478152991b194c79764e5ea0
Chloe Kudryavtsev <toast@toast.cafe>
2020-04-13 08:30:46 -0400

    zsh: add alias list

---
+++ dot_zsh/source/alias.zsh
@@ -0,0 +1,9 @@
+alias cat=bat
+alias cp='cp --reflink=auto'
+alias dif='colordiff -u'
+alias g=genius
+alias less='less -R'
+alias m=micro
+alias ossh='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR'
+alias tar=bsdtar
+alias vim=nvim
