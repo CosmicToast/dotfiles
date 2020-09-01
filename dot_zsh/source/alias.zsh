7d27320e03de5f2a5cce32a9140c35b05a0bdb71
Chloe Kudryavtsev <toast@toast.cafe>
2020-09-01 17:11:57 -0400

    zsh: clean up aliases a bit, add ht

--- dot_zsh/source/alias.zsh
+++ dot_zsh/source/alias.zsh
@@ -1,6 +1,6 @@
 alias cp='cp --reflink=auto'
-alias dit='docker run --rm -it'
-alias drun='dit -v "$PWD":/pwd:Z -w /pwd'
+alias dit='docker run --rm -it' drun='dit -v "$PWD":/pwd:Z -w /pwd'
+alias h='ht -Fb' ht='ht -F'
 alias g=genius
 alias less='less -R'
 alias m=micro
@@ -9,5 +9,4 @@ alias tar=bsdtar
 alias vim=nvim
 alias yaegi='rlwrap yaegi'
 
-alias bshar='bsdtar --format=shardump'
-alias shar='bsdtar --format=shar'
+alias bshar='bsdtar --format=shardump' shar='bsdtar --format=shar'
