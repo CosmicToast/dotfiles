a92992118efeb252911c5de7c604f407f0c399b0
Chloe Kudryavtsev <toast@toast.cafe>
2020-09-04 16:19:02 -0400

    zsh: automatically import systemd env if missing
    the check is against TERMINFO_DIRS - it'll get added in a second I promise

--- dot_zsh/source/env.zsh
+++ dot_zsh/source/env.zsh
@@ -1,3 +1,9 @@
 export EDITOR==micro
 export GIT_EDITOR==micro
 export PAGER='less -R'
+
+if [[ -z $TERMINFO_DIRS ]]; then
+	autoload -Uz autosource
+	# maybe others later!
+	autosource import-systemd-env
+fi
