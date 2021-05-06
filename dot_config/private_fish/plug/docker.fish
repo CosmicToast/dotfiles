a6e0fadf068f0623c7ef6d70f0949f479044a6cc
Chloe Kudryavtsev <toast@toast.cafe>
2021-05-06 12:23:36 -0400

    fish: make docker plugin less likely to fail

--- dot_config/private_fish/plug/docker.fish
+++ dot_config/private_fish/plug/docker.fish
@@ -2,6 +2,6 @@ type -qf podman
 and alias docker podman
 
 if type -q docker
-    alias dit  'docker run --rm -it'
+    alias dit  'docker run -it --rm'
     alias drun 'dit -v (pwd):/pwd:Z -w /pwd'
 end
