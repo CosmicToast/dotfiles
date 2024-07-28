43bf4c4fa4b98cf06d0a7a8e14c7aed617f1eb95
Chloé Vulquin <code@toast.bunkerlabs.net>
2024-07-28 10:13:57 +0200

    nvim/neotree: update to 3.x

--- dot_config/nvim/fnl/plugins/neo-tree.fnl
+++ dot_config/nvim/fnl/plugins/neo-tree.fnl
@@ -1,5 +1,6 @@
 (import-macros {:mixed-table · : recc} :toast.macros)
 [(· :nvim-neo-tree/neo-tree.nvim &
+    :branch :v3.x
     :cmd :Neotree
     :dependencies [:nvim-lua/plenary.nvim
                    :MunifTanjim/nui.nvim]
--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/neo-tree.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
-  return (require("neo-tree.command")).execute({toggle = true})
+  return require("neo-tree.command").execute({toggle = true})
 end
-return {{"nvim-neo-tree/neo-tree.nvim", cmd = "Neotree", dependencies = {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim"}, keys = {{"<leader>n", _1_, desc = "Neotree"}}, opts = {}}}
+return {{"nvim-neo-tree/neo-tree.nvim", branch = "v3.x", cmd = "Neotree", dependencies = {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim"}, keys = {{"<leader>n", _1_, desc = "Neotree"}}, opts = {}}}
