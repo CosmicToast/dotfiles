5fb2d4b43703dabf217e59bcdfc1f171e0722792
Chloé Vulquin <code@toast.bunkerlabs.net>
2024-08-06 13:19:05 +0200

    nvim: move plugins/bindings into a single file
    The plan from now on will be "single big file" unless there's more
    complex things going on, such as with lsp.

---
+++ dot_config/nvim/lua/plugins/bindings.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/bindings.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"folke/which-key.nvim", lazy = true, opts = {}}, {"tpope/vim-rsi"}}
--- dot_config/nvim/lua/plugins/bindings/init.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/bindings/init.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"folke/which-key.nvim", lazy = true, opts = {}}, {"tpope/vim-rsi"}}
