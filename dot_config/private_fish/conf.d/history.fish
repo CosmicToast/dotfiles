e662c914e73f46b0baa2857e904412046dae3e60
Chloé Vulquin <code@toast.bunkerlabs.net>
2024-12-12 16:03:45 +0100

    fish/history: node_history -> node
    No idea how this happened.

--- dot_config/private_fish/conf.d/history.fish
+++ dot_config/private_fish/conf.d/history.fish
@@ -3,7 +3,7 @@
 # I want all my history in ~/.local/share/history/, so…
 set -Uuq RLWRAP_HOME || set -Uu RLWRAP_HOME ~/.local/share/history
 set -Uuq LESSHISTFILE || set -Uu LESSHISTFILE $RLWRAP_HOME/less
-set -Uuq NODE_REPL_HISTORY || set -Uu NODE_REPL_HISTORY $RLWRAP_HOME/node_history
+set -Uuq NODE_REPL_HISTORY || set -Uu NODE_REPL_HISTORY $RLWRAP_HOME/node
 set -Uuq PSQL_HISTORY || set -Uu PSQL_HISTORY $RLWRAP_HOME/psql
 set -Uuq PYTHON_HISTORY || set -Uu PYTHON_HISTORY $RLWRAP_HOME/python
 set -Uuq SQLITE_HISTORY || set -Uu SQLITE_HISTORY $RLWRAP_HOME/sqlite
