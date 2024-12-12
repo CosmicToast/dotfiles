8fc21cbcf627b552b30803c02bed1393ca68d31a
Chloé Vulquin <code@toast.bunkerlabs.net>
2024-12-12 16:16:21 +0100

    fish/history: move history to state dir

--- dot_config/private_fish/conf.d/history.fish
+++ dot_config/private_fish/conf.d/history.fish
@@ -1,7 +1,7 @@
 # a bunch of items pollute ~ with history files
 # and they all want different stuff to not do that!
 # I want all my history in ~/.local/share/history/, so…
-set -Uuq RLWRAP_HOME || set -Uu RLWRAP_HOME ~/.local/share/history
+set -Uuq RLWRAP_HOME || set -Uu RLWRAP_HOME ~/.local/state/history
 set -Uuq LESSHISTFILE || set -Uu LESSHISTFILE $RLWRAP_HOME/less
 set -Uuq NODE_REPL_HISTORY || set -Uu NODE_REPL_HISTORY $RLWRAP_HOME/node
 set -Uuq PSQL_HISTORY || set -Uu PSQL_HISTORY $RLWRAP_HOME/psql
