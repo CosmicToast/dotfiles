2620f1268d6b7dfdc522460248cb1fb4bfc57bbc
Chloé Vulquin <code@toast.bunkerlabs.net>
2025-05-13 16:35:26 +0200

    fish: also domicile rustup in ~/.local/rust

--- dot_config/private_fish/conf.d/rust.fish
+++ dot_config/private_fish/conf.d/rust.fish
@@ -1,2 +1,3 @@
-set -Uuq CARGO_HOME || set -Uu CARGO_HOME ~/.local/rust
-setux CARGO_HOME
+set -Uuq CARGO_HOME  || set -Uu CARGO_HOME  ~/.local/rust
+set -Uuq RUSTUP_HOME || set -Uu RUSTUP_HOME ~/.local/rust
+setux CARGO_HOME RUSTUP_HOME
