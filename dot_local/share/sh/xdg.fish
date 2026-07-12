509baad095d168fb3dd93e92cc224c24089c3246
Chloé Vulquin <toast@bunkerlabs.net>
2026-07-12 20:58:18 +0200

    sh/xdg: also make ~/.local configurable
    In case I make it .opt or something idk.

--- dot_local/share/sh/xdg.fish
+++ dot_local/share/sh/xdg.fish
@@ -1,34 +1,35 @@
 #!/bin/sh
-set -q XDG_CONFIG_HOME || set -gx XDG_CONFIG_HOME ~/.local/etc
-set -q XDG_DATA_HOME   || set -gx XDG_DATA_HOME   ~/.local/share
-set -q XDG_STATE_HOME  || set -gx XDG_STATE_HOME  ~/.local/var
-set -q XDG_CACHE_HOME  || set -gx XDG_CACHE_HOME  ~/.local/tmp
+set -q XDG_OPT         || set -l  XDG_OPT         ~/.local
+set -q XDG_CONFIG_HOME || set -gx XDG_CONFIG_HOME $XDG_OPT/etc
+set -q XDG_DATA_HOME   || set -gx XDG_DATA_HOME   $XDG_OPT/share
+set -q XDG_STATE_HOME  || set -gx XDG_STATE_HOME  $XDG_OPT/var
+set -q XDG_CACHE_HOME  || set -gx XDG_CACHE_HOME  $XDG_OPT/tmp
 
 # XDG compatiblity settings for everything
 
-set -gx BUN_INSTALL ~/.local/bun
+set -gx BUN_INSTALL $XDG_OPT/bun
 
 set -gx CLJ_CACHE  $XDG_CACHE_HOME/clojure
 set -gx CLJ_CONFIG $XDG_CONFIG_HOME/clojure
 
-set -gx DOTNET_CLI_HOME ~/.local/dotnet
+set -gx DOTNET_CLI_HOME $XDG_OPT/dotnet
 
-set -gx JULIA_DEPOT_PATH   ~/.local/julia
-set -gx JULIAUP_DEPOT_PATH ~/.local/julia
+set -gx JULIA_DEPOT_PATH   $XDG_OPT/julia
+set -gx JULIAUP_DEPOT_PATH $XDG_OPT/julia
 
-set -gx OPAMROOT ~/.local/ocaml
+set -gx OPAMROOT $XDG_OPT/ocaml
 
-set -gx IPYTHONDIR            ~/.local/python/ipython
-set -gx JUPYTER_CONFIG_DIR    ~/.local/python/jupyter
-set -gx UV_PYTHON_BIN_DIR     ~/.local/python/bin
-set -gx UV_PYTHON_INSTALL_DIR ~/.local/python/interpreters
-set -gx UV_TOOL_BIN_DIR       ~/.local/python/bin
-set -gx UV_TOOL_DIR           ~/.local/python/tools
+set -gx IPYTHONDIR            $XDG_OPT/python/ipython
+set -gx JUPYTER_CONFIG_DIR    $XDG_OPT/python/jupyter
+set -gx UV_PYTHON_BIN_DIR     $XDG_OPT/python/bin
+set -gx UV_PYTHON_INSTALL_DIR $XDG_OPT/python/interpreters
+set -gx UV_TOOL_BIN_DIR       $XDG_OPT/python/bin
+set -gx UV_TOOL_DIR           $XDG_OPT/python/tools
 
-set -gx CARGO_HOME  ~/.local/rust
-set -gx RUSTUP_HOME ~/.local/rust
+set -gx CARGO_HOME  $XDG_OPT/rust
+set -gx RUSTUP_HOME $XDG_OPT/rust
 
-set -gx ZVM_PATH ~/.local/zig
+set -gx ZVM_PATH $XDG_OPT/zig
 
 set -gx RLWRAP_HOME $XDG_STATE_HOME/history
 set -gx LESSHIST_FILE     $RLWRAP_HOME/less
